d6997583f17a2c6d99af046880c3d177b95b53d8
ojumbo <69497447+ojumbo@users.noreply.github.com>
2021-01-11 23:30:43 +0900

    Add files via upload

---
+++ main.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>A.O.T main</title>
+<style>
+body{
+  background-image: url("doc/munc.gif");
+}
+</style>
+  </head>
+  <body>
+  <h2>  <p>
+  <?php  echo "Attack On Titan" ?>
+</h2>  </p>
+    <p>
+      <audio src="doc/홍련의화살.mp3" autoplay="autoplay" controls="controls">
+      </audio>
+    </p>
+    <h2>
+<a href="titans1.php"> START </a>
+</h2>
+  </body>
+</html>
---
+++ titans1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>
+    <audio src="doc/youseebiggirl.mp3" autoplay="autoplay" controls="controls" >
+    </p>
+    <img src="doc/titans1.gif" alt="대충 뛰어다니는 짤" width="800px" height="800px">
+  </body>
+</html>
